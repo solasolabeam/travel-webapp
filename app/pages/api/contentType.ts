8b0153de4cd2de4b1c63dc98886977dd8984ed63
sol <nathf1005@naver.com>
2024-10-14 10:14:44 +0900

    redux 전역변수 작성

---
+++ app/pages/api/contentType.ts
@@ -0,0 +1,41 @@
+let getContentType: { code: number, name: string, url: string }[] = [
+    {
+        code: 12,
+        name: '관광지',
+        url: '/tour'
+    }, {
+        code: 14,
+        name: '문화시설',
+        url: '/culture'
+    }, {
+        code: 15,
+        name: '행사/공연/축제',
+        url: '/event'
+    },
+    // {
+    //     code : 25,
+    //     name : '여행코스'
+    // },
+    // {
+    //     code: 28,
+    //     name: '레포츠',
+    //     url: '/leports'
+    // }, 
+    {
+        code: 32,
+        name: '숙박',
+        url: '/hotel'
+    },
+    // {
+    //     code: 38,
+    //     name: '쇼핑',
+    //     url: '/shopping'
+    // }, 
+    // {
+    //     code: 39,
+    //     name: '음식점',
+    //     url: '/food'
+    // }
+];
+
+export default getContentType;
--- app/store.ts
+++ app/store.ts
@@ -1,32 +1,186 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
+import getContentType from './pages/api/contentType';
 
 interface num {
   value: number;
 }
 
-const initialState: num = {
-  value: 0,
-};
 
-export const counterSlice = createSlice({
+let counter = createSlice({
   name: 'counter',
-  initialState,
+  initialState: 0,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1
   },
 });
 
-export let { increment, decrement } = counterSlice.actions
+let contentType = createSlice({
+  name: 'contentType', //state이름 ~
+  initialState: getContentType, //값
+  reducers: {
+  }
+})
+
+let sido = createSlice({
+  name: 'sido', // state 이름
+  initialState: [], // 초기 상태값
+  reducers: {
+    changeSido(state, action) {
+      // Immer가 상태 불변성을 관리하므로 상태를 직접 수정해도 됨
+      // 또는 state.push(...action.payload) 처럼 작성 가능
+      return action.payload; 
+    }
+  }
+});
+
+let sidoVal = createSlice({
+  name: 'sidoVal', //state이름 ~
+  initialState: '', //값
+  reducers: {
+    changeSidoVal(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let gugun = createSlice({
+  name: 'gugun', //state이름 ~
+  initialState: [], //값
+  reducers: {
+    changeGugun(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let gugunVal = createSlice({
+  name: 'gugunVal', //state이름 ~
+  initialState: '', //값
+  reducers: {
+    changeGugunVal(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let contentTypeVal = createSlice({
+  name: 'contentTypeVal',
+  initialState: '',
+  reducers: {
+    changeContentTypeVal(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let cat1Val = createSlice({
+  name: 'cat1Val',
+  initialState: '',
+  reducers: {
+    changeCat1CVal(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let cat2Val = createSlice({
+  name: 'cat1Val',
+  initialState: '',
+  reducers: {
+    changeCat2CVal(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let cat3Val = createSlice({
+  name: 'cat1Val',
+  initialState: '',
+  reducers: {
+    changeCat3CVal(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let headerSearch = createSlice({
+  name: 'headerSearch',
+  initialState: [],
+  reducers: {
+    changeHeaderSearch(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let keyword = createSlice({
+  name: 'keyword',
+  initialState: '',
+  reducers: {
+    changeKeyword(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let addRow = createSlice({
+  name: 'addRow',
+  initialState: 1,
+  reducers: {
+    changeRow(state, action) {
+      return action.payload
+    }
+  }
+})
+
+let bannerIdx = createSlice({
+  name: 'bannerIdx',
+  initialState: '0',
+  reducers: {
+    changeBanner(state, action) {
+      return action.payload
+    }
+  }
+})
+
+export let { changeSido } = sido.actions
+export let { changeSidoVal } = sidoVal.actions
+export let { changeGugun } = gugun.actions
+export let { changeGugunVal } = gugunVal.actions
+
+export let { changeContentTypeVal } = contentTypeVal.actions
+export let { changeCat1CVal } = cat1Val.actions
+export let { changeCat2CVal } = cat2Val.actions
+export let { changeCat3CVal } = cat3Val.actions
+export let { changeHeaderSearch } = headerSearch.actions
+export let { changeKeyword } = keyword.actions
+
+export let { changeRow } = addRow.actions
+export let { changeBanner } = bannerIdx.actions
+
+
+export let { increment, decrement } = counter.actions
 
 
 export const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
+    counter: counter.reducer,
+    contentType: contentType.reducer,
+    sido: sido.reducer,
+    sidoVal: sidoVal.reducer,
+    gugun: gugun.reducer,
+    gugunVal: gugunVal.reducer,
+
+    contentTypeVal: contentTypeVal.reducer,
+    cat1Val: cat1Val.reducer,
+    cat2Val: cat2Val.reducer,
+    cat3Val: cat3Val.reducer,
+    headerSearch: headerSearch.reducer,
+    keyword: keyword.reducer,
+
+    addRow: addRow.reducer,
+    bannerIdx: bannerIdx.reducer
   },
 });
 
