b14d4f364704d378fa73cd5fc9389b99fea5f804
sol <nathf1005@naver.com>
2024-10-17 01:26:07 +0900

    raw data 생성

---
+++ app/_data/bannerData.ts
@@ -0,0 +1,24 @@
+const getBannerData = [
+    {
+        bgColor: '#b8dfff',
+        title: '숲, 정원, 호수, 습지가 공존하는 공간',
+        tag: '#서울  &nbsp;&nbsp;&nbsp; #강서구 &nbsp;&nbsp;&nbsp; #관광지 &nbsp;&nbsp;&nbsp; #수목원 &nbsp;&nbsp;&nbsp;',
+    },
+    {
+        bgColor: '#ffd9d9',
+        title: '일상의 궤도를 벗어나는 설렘 가득한 여정을 떠나보세요',
+        tag: '#인천광역시  &nbsp;&nbsp;&nbsp; #중구 &nbsp;&nbsp;&nbsp; #문화시설 &nbsp;&nbsp;&nbsp; #전시회 &nbsp;&nbsp;&nbsp;',
+    },
+    {
+        bgColor: '#c2b2f8',
+        title: '국내 최대 규모의 초호화 이사부크루즈',
+        tag: '#전라남도  &nbsp;&nbsp;&nbsp; #여수시 &nbsp;&nbsp;&nbsp; #축제 &nbsp;&nbsp;&nbsp; #기타행사 &nbsp;&nbsp;&nbsp;',
+    },
+    {
+        bgColor: '#869fee',
+        title: '현대적인 모듈식 디자인과 모든 장비를 갖춘 완벽한 장소',
+        tag: '#서울  &nbsp;&nbsp;&nbsp; #중구 &nbsp;&nbsp;&nbsp; #숙박 &nbsp;&nbsp;&nbsp; #관광호텔 &nbsp;&nbsp;&nbsp;',
+    },
+]
+
+export default getBannerData;
