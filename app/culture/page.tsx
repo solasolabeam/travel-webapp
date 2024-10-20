4c481b21083323317a65391cf161f678d82253ab
sol <nathf1005@naver.com>
2024-10-21 01:44:50 +0900

    배너바 컴포넌트 생성

--- app/culture/page.tsx
+++ app/culture/page.tsx
@@ -1,8 +1,10 @@
 
+// import BannerBar from "../_component/BannerBar"
 import SidoGugun from "../_component/SidoGugun"
 export default function Culture() {
   return (
     <>
+      {/* <BannerBar /> */}
       <SidoGugun />
     </>
   )
