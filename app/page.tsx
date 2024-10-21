d0a307601d84cad6fb07e0c2261a0465e6ea7459
sol <nathf1005@naver.com>
2024-10-21 14:03:42 +0900

    react-query install

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Banner from './_component/Banner';
 import RecommendPart from './_component/RecommendPart';
 
 export default function Home() {
   return (
     <>
-      <Banner />
       <RecommendPart />
     </>
   );
