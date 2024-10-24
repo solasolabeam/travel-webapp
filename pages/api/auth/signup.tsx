542733244f41eb729eb49398528f4f804abe8ae7
sol <nathf1005@naver.com>
2024-10-24 22:19:40 +0900

    변수 수정

--- pages/api/auth/signup.tsx
+++ pages/api/auth/signup.tsx
@@ -7,7 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const hash = await bcrypt.hash(data.password, 10)
         data.password = hash
         const db = (await connectDB).db('trip');
-        await db.collection('user_cred').insertOne(req.body)
+        await db.collection('user_cred').insertOne(data)
         res.status(200).json({ msg: 'success' })
     }
 }
