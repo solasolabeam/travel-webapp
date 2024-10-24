87c5f0eb64877d351298b790dd267da8c6e2920a
sol <nathf1005@naver.com>
2024-10-24 16:21:27 +0900

    마이페이지 에서 배너 disabled

--- app/_component/Banner.tsx
+++ app/_component/Banner.tsx
@@ -2,22 +2,30 @@
 import Slide from "./Slide";
 import getBannerData from "../_data/bannerData"
 import { useAppSelector } from "../hooks";
+import { usePathname } from "next/navigation";
 
 
 export default function Banner() {
     const bannerIdx = useAppSelector(state => state.bannerIdx)
+    const pathname = usePathname()
     return (
-        <div className="header-bg-container" style={{ background: getBannerData[bannerIdx].bgColor }}>
-            <div className="header-bg-area">
-                <div className="header-bg-left">
-                    <p>{getBannerData[bannerIdx].title}</p>
-                    <p dangerouslySetInnerHTML={{ __html: getBannerData[bannerIdx].tag }}></p>
+        <>
+            {
+                pathname !== '/mypage'
+                &&
+                <div className="header-bg-container" style={{ background: getBannerData[bannerIdx].bgColor }}>
+                    <div className="header-bg-area">
+                        <div className="header-bg-left">
+                            <p>{getBannerData[bannerIdx].title}</p>
+                            <p dangerouslySetInnerHTML={{ __html: getBannerData[bannerIdx].tag }}></p>
+                        </div>
+                        <div className="header-bg-right">
+                            {/* 배너 슬라이드 */}
+                            <Slide />
+                        </div>
+                    </div>
                 </div>
-                <div className="header-bg-right">
-                    {/* 배너 슬라이드 */}
-                    <Slide />
-                </div>
-            </div>
-        </div>
+            }
+        </>
     )
 }
